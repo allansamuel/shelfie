e68eee71e7143d22277fbd723e0ca77d29103afe
allansamuel <allan.samuelg@gmail.com>
2021-04-16 21:37:48 -0300

    create shelfie server

---
+++ server/shelfie-server/src/main/java/com/shelfie/ShelfieServerApplication.java
@@ -0,0 +1,13 @@
+package com.shelfie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfieServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelfieServerApplication.class, args);
+	}
+
+}
