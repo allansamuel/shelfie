09cf0991c2970807c5d2ab3e073e786a0f80126d
roberta-teixeira <robertabteixeira02@gmail.com>
2021-04-17 00:12:51 -0300

    create interactive book

---
+++ server/shelfie-server/src/main/java/model/InteractiveBook.java
@@ -0,0 +1,18 @@
+package model;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "interactive_book")
+public class InteractiveBook {
+
+
+	
+}
