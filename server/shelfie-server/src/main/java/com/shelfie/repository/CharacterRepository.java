3e4c6eceb032cf9df20574207c04a6527698aa3c
allansamuel <allan.samuelg@gmail.com>
2021-04-18 17:11:25 -0300

    fix character repository class import

--- server/shelfie-server/src/main/java/com/shelfie/repository/CharacterRepository.java
+++ server/shelfie-server/src/main/java/com/shelfie/repository/CharacterRepository.java
@@ -2,6 +2,8 @@ package com.shelfie.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.shelfie.model.Character;
+
 public interface CharacterRepository extends JpaRepository<Character, Integer>{
 
 }
