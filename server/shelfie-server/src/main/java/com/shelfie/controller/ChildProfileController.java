b8e9853b116eadf1bc34cb8583b9da01d6d7cf19
allansamuel <allan.samuelg@gmail.com>
2021-04-20 20:36:01 -0300

    create post and put childProfile routes

--- server/shelfie-server/src/main/java/com/shelfie/controller/ChildProfileController.java
+++ server/shelfie-server/src/main/java/com/shelfie/controller/ChildProfileController.java
@@ -1,35 +1,42 @@
 package com.shelfie.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.shelfie.model.ChildProfile;
 import com.shelfie.repository.ChildProfileRepository;
+
+import javassist.NotFoundException;
 
 @Controller
 @RequestMapping("child_profiles")
 public class ChildProfileController {
 
 	@Autowired
-	private ChildProfileRepository repo;
+	private ChildProfileRepository childProfileRepository;
 	
 	@GetMapping
 	@ResponseBody
-	public List<ChildProfile> getAll(){
-		return repo.findAll();
+	public List<ChildProfile> getAll() {
+		return childProfileRepository.findAll();
 	}
 	
-	@GetMapping("{childProfileId}")
+	@GetMapping("{id}")
 	@ResponseBody
-	public Optional<ChildProfile> getById(@RequestParam Integer childProfileId){
-		return repo.findById(childProfileId);
+	public ChildProfile getById(@PathVariable(value = "id") Integer childProfileId) throws Exception {
+		return childProfileRepository.findById(childProfileId)
+				.orElseThrow(() -> new Exception());
 	}
 	
 	/*
@@ -39,4 +46,37 @@ public class ChildProfileController {
 		return repo.findByGuardianUser(guardianUserId);
 	}
 	*/
+	
+	@PostMapping
+	@ResponseBody
+	public ResponseEntity<ChildProfile> create(@RequestBody ChildProfile childProfileBody) throws Exception {
+		try {
+			ChildProfile newChildProfile = childProfileRepository.save(childProfileBody);
+			return ResponseEntity.ok(newChildProfile);
+		} catch (Exception exception) {
+			throw exception;
+		}
+	}
+	
+	@PutMapping("{id}")
+	@ResponseBody
+	public ResponseEntity<ChildProfile> edit(@RequestBody ChildProfile childProfileBody, 
+			@PathVariable(value = "id") Integer childProfileId) throws Exception {
+		ChildProfile childProfile = childProfileRepository.findById(childProfileId)
+				.orElseThrow(() -> new NotFoundException("not found"));
+		
+		childProfile.setNickname(childProfileBody.getNickname());
+		childProfile.setCharacter(childProfileBody.getCharacter());
+		
+		ChildProfile updatedChildProfile = childProfileRepository.save(childProfile);
+	    return ResponseEntity.ok(updatedChildProfile);
+	}
+	
+	@DeleteMapping("{id}")
+	public void delete(@PathVariable(value = "id") Integer childProfileId) throws Exception {
+		ChildProfile childProfile = childProfileRepository.findById(childProfileId)
+				.orElseThrow(() -> new NotFoundException("not found"));
+		
+		childProfileRepository.delete(childProfile);
+	}
 }
