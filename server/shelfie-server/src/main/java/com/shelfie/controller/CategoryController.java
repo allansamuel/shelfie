bc17795ff06996bbc29350187c92b492fc0ad02b
roberta-teixeira <robertabteixeira02@gmail.com>
2021-04-20 23:52:09 -0300

    create category controller and update path

--- server/shelfie-server/src/main/java/com/shelfie/controller/CategoryController.java
+++ server/shelfie-server/src/main/java/com/shelfie/controller/CategoryController.java
@@ -1,7 +1,6 @@
 package com.shelfie.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,39 +10,37 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.shelfie.model.Character;
-import com.shelfie.repository.CharacterRepository;
-
+import com.shelfie.model.Category;
+import com.shelfie.repository.CategoryRepository;
 import javassist.NotFoundException;
 
 @RestController
-@RequestMapping("character")
-public class CharacterController {
-	
+@RequestMapping("category")
+public class CategoryController {
+
 	@Autowired
-	private CharacterRepository characterRepository;
+	private CategoryRepository categoryRepository;
 
 	@GetMapping("{id}")
-	public ResponseEntity<Character> getCharacterById(@PathVariable Integer id) throws Exception {
-	 Character character = characterRepository.findById(id)
+	public ResponseEntity<Category> getCategoryById(@PathVariable Integer id) throws Exception {
+		Category category = categoryRepository.findById(id)
 			 .orElseThrow(() -> new NotFoundException ("not found" + id));
-	 return ResponseEntity.ok().body(character);
+	 return ResponseEntity.ok().body(category);
 	}
 	
 	@GetMapping
-	public List <Character> getAll(){
-	 List <Character> character = characterRepository.findAll();
-	 return character;
+	public List <Category> getAll(){
+	 List <Category> category = categoryRepository.findAll();
+	 return category;
 	}
 	
 	@PostMapping
-	public Character createCharacter(@RequestBody Character character) throws Exception{
+	public Category createCategory (@RequestBody Category  category ) throws Exception{
 		try {
-		Character newCharacter = characterRepository.save(character);
-		return characterRepository.save(newCharacter); 
+			Category newCategory  = categoryRepository.save(category );
+		return categoryRepository.save(newCategory ); 
 		} catch (Exception exception) {
 			throw exception;
 		} 
 	}
-	
 }
--- server/shelfie-server/src/main/java/com/shelfie/controller/QuestController.java
+++ server/shelfie-server/src/main/java/com/shelfie/controller/QuestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ import com.shelfie.repository.QuestRepository;
 import javassist.NotFoundException;
 
 @RestController
+@RequestMapping("quest")
 public class QuestController {
 	
 	@Autowired
