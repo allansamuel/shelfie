f5812a2c9627fe9c88844cdb6a4f0664c24913d9
allansamuel <allan.samuelg@gmail.com>
2021-04-24 00:46:48 -0300

    start android application

---
+++ client/app/src/main/java/com/shelfie/ui/registerForm/RegisterFormActivity.java
@@ -0,0 +1,14 @@
+package com.shelfie.ui.registerForm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_form);
+    }
+}
