b8333f8b8a8186831e857985a66042ed967fae14
allansamuel <allan.samuelg@gmail.com>
2021-04-28 22:30:45 -0300

    implement saaripar validation

--- client/app/src/main/java/com/shelfie/ui/userregister/FormGuardianUserActivity.java
+++ client/app/src/main/java/com/shelfie/ui/userregister/FormGuardianUserActivity.java
@@ -1,16 +1,12 @@
 package com.shelfie.ui.userregister;
 
 import androidx.appcompat.app.AppCompatActivity;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.mobsandgeeks.saripaar.ValidationError;
@@ -21,16 +17,14 @@ import com.mobsandgeeks.saripaar.annotation.Length;
 import com.mobsandgeeks.saripaar.annotation.NotEmpty;
 import com.mobsandgeeks.saripaar.annotation.Password;
 import com.shelfie.R;
-import com.shelfie.config.RetrofitConfig;
 import com.shelfie.model.GuardianUser;
-import com.shelfie.service.GuardianUserService;
 
 import java.util.List;
+import java.util.Objects;
 
 public class FormGuardianUserActivity extends AppCompatActivity implements Validator.ValidationListener {
 
-    private RetrofitConfig retrofitConfig;
-    private GuardianUserService guardianUserService;
+    private Validator formValidator;
     private GuardianUser guardianUser;
 
     private TextInputLayout txtGuardianUserName;
@@ -62,18 +56,13 @@ public class FormGuardianUserActivity extends AppCompatActivity implements Valid
 
         init();
 
-        btnGuardianUserNext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                createGuardianUser();
-            }
+        btnGuardianUserNext.setOnClickListener(view -> {
+            resetErrors();
+            formValidator.validate();
         });
     }
 
     private void init() {
-        retrofitConfig = new RetrofitConfig();
-        guardianUserService = retrofitConfig.getGuardianUserService();
-
         txtGuardianUserName = findViewById(R.id.txt_guardian_user_name);
         etGuardianUserName = findViewById(R.id.et_guardian_user_name);
         txtGuardianUserEmail = findViewById(R.id.txt_guardian_user_email);
@@ -85,40 +74,54 @@ public class FormGuardianUserActivity extends AppCompatActivity implements Valid
         btnGuardianUserNext = findViewById(R.id.btn_guardian_user_next);
     }
 
-    private void createGuardianUser() {
+    private void startChildProfileFormIntent() {
+        formValidator = new Validator(this);
         guardianUser = new GuardianUser();
-        guardianUser.setGuardianUserName(etGuardianUserName.getText().toString());
-        guardianUser.setGuardianUserEmail(etGuardianUserEmail.getText().toString());
-        guardianUser.setGuardianUserPassword(etGuardianUserPassword.getText().toString());
+        guardianUser.setGuardianUserName(Objects.requireNonNull(etGuardianUserName.getText()).toString());
+        guardianUser.setGuardianUserEmail(Objects.requireNonNull(etGuardianUserEmail.getText()).toString());
+        guardianUser.setGuardianUserPassword(Objects.requireNonNull(etGuardianUserPassword.getText()).toString());
 
-        guardianUserService.create(guardianUser).enqueue(new Callback<GuardianUser>() {
-            @Override
-            public void onResponse(Call<GuardianUser> call, Response<GuardianUser> response) {
-                if(response.isSuccessful()) {
-                    Intent intent = new Intent(getApplicationContext(), ManageChildProfileActivity.class);
-                    Bundle bundle = new Bundle();
-                    bundle.putSerializable("GUARDIAN_USER_DATA", response.body());
-                    intent.putExtras(bundle);
-                    startActivity(intent);
-                } else {
-                    Snackbar.make(getWindow().getDecorView().getRootView(), "nao deu colega", Snackbar.LENGTH_LONG).show();
-                }
-            }
+        Intent intent = new Intent(getApplicationContext(), ManageChildProfileActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(getString(R.string.bundle_guardian_user), guardianUser);
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
 
-            @Override
-            public void onFailure(Call<GuardianUser> call, Throwable t) {
-                Snackbar.make(getWindow().getDecorView().getRootView(), t.getMessage(), Snackbar.LENGTH_LONG).show();
-            }
-        });
+    private void resetErrors(){
+        txtGuardianUserName.setError("");
+        txtGuardianUserEmail.setError("");
+        txtGuardianUserPassword.setError("");
+        txtGuardianUserPasswordConfirm.setError("");
     }
 
     @Override
     public void onValidationSucceeded() {
-
+        startChildProfileFormIntent();
     }
 
     @Override
     public void onValidationFailed(List<ValidationError> errors) {
+        for(ValidationError e:errors) {
+            View view = e.getView();
+            String errorMessage = e.getCollatedErrorMessage(this);
 
+            if(view instanceof TextInputEditText){
+                switch (view.getId()){
+                    case R.id.et_guardian_user_name:
+                        txtGuardianUserName.setError(errorMessage);
+                        break;
+                    case R.id.et_guardian_user_email:
+                        txtGuardianUserEmail.setError(errorMessage);
+                        break;
+                    case R.id.et_guardian_user_password:
+                        txtGuardianUserPassword.setError(errorMessage);
+                        break;
+                    case R.id.et_guardian_user_password_confirm:
+                        txtGuardianUserPasswordConfirm.setError(errorMessage);
+                        break;
+                }
+            }
+        }
     }
 }
