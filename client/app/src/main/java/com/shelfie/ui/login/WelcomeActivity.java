fd5610e54b582d220da2125226297233f8780893
allansamuel <allan.samuelg@gmail.com>
2021-05-13 21:17:41 -0300

    [client] finish welcome/loading activity

--- client/app/src/main/java/com/shelfie/ui/login/WelcomeActivity.java
+++ client/app/src/main/java/com/shelfie/ui/login/WelcomeActivity.java
@@ -2,18 +2,50 @@ package com.shelfie.ui.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.shelfie.R;
+import com.shelfie.model.GuardianUser;
+import com.shelfie.ui.userregister.ManageChildProfileActivity;
+import com.shelfie.utils.UserSession;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    private ImageView imgWelcomeShelfieLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        Intent a = new Intent(getApplicationContext(), LoginActivity.class);
-        startActivity(a);
+
+        init();
+        verifyUserSession();
+    }
+
+    private void init() {
+        imgWelcomeShelfieLogo = findViewById(R.id.img_welcome_shelfie_logo);
+        ObjectAnimator pulseAnimation = ObjectAnimator.ofPropertyValuesHolder(
+                imgWelcomeShelfieLogo,
+                PropertyValuesHolder.ofFloat("scaleX", 1.08f),
+                PropertyValuesHolder.ofFloat("scaleY", 1.08f));
+        pulseAnimation.setDuration(500);
+
+        pulseAnimation.setRepeatCount(ObjectAnimator.INFINITE);
+        pulseAnimation.setRepeatMode(ObjectAnimator.REVERSE);
+        pulseAnimation.start();
+    }
+
+    private void verifyUserSession() {
+        if(UserSession.getGuardianUser(getApplicationContext()) != null) {
+            startActivity(new Intent(getApplicationContext(), ManageChildProfileActivity.class));
+        } else {
+            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+        }
     }
 }
