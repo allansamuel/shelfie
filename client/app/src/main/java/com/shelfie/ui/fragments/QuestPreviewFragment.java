78705c2f4216f9178484a227afbdc73ad2a30754
allansamuel <allan.samuelg@gmail.com>
2021-05-18 18:37:05 -0300

    [client] update quest style if children has completed it

--- client/app/src/main/java/com/shelfie/ui/fragments/QuestPreviewFragment.java
+++ client/app/src/main/java/com/shelfie/ui/fragments/QuestPreviewFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -20,6 +21,7 @@ public class QuestPreviewFragment extends Fragment {
     private static final String ARG_QUEST = "QUEST_DATA";
 
     private Quest quest;
+    private CardView cvQuestPreviewContainer;
     private ImageView imgQuestIcon;
     private TextView tvQuestTitle;
     private TextView tvQuestDescription;
@@ -57,15 +59,27 @@ public class QuestPreviewFragment extends Fragment {
 
     private void init() {
         View view = getView();
+        cvQuestPreviewContainer = view.findViewById(R.id.cv_quest_preview_container);
+        imgQuestIcon = view.findViewById(R.id.img_quest_icon);
         tvQuestTitle = view.findViewById(R.id.tv_quest_title);
         tvQuestDescription = view.findViewById(R.id.tv_quest_description);
         tvQuestReward = view.findViewById(R.id.tv_quest_reward);
 
-        if(quest != null) {
-            tvQuestTitle.setText(quest.getQuestTitle());
-            tvQuestDescription.setText(quest.getQuestDescription());
-            tvQuestReward.setText(String.valueOf(quest.getCoinsReward()));
-        }
+        tvQuestTitle.setText(quest.getQuestTitle());
+        tvQuestDescription.setText(quest.getQuestDescription());
+        tvQuestReward.setText(String.valueOf(quest.getCoinsReward()));
 
+        if(true) { //substituir por serviço de verificação se a missão está concluída
+            setQuestCompleteStyle();
+        }
+    }
+
+    private void setQuestCompleteStyle() {
+        cvQuestPreviewContainer.setCardBackgroundColor(getContext().getColor(R.color.green_400));
+        imgQuestIcon.setImageDrawable(getContext().getDrawable(R.drawable.quest_icon_finished));
+        tvQuestTitle.setTextColor(getContext().getColor(R.color.design_default_color_on_primary));
+        tvQuestDescription.setTextColor(getContext().getColor(R.color.design_default_color_on_primary));
+        tvQuestReward.setCompoundDrawablesWithIntrinsicBounds
+                (null, null, getContext().getDrawable(R.drawable.child_profile_coin_outlined), null);
     }
 }
