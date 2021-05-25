c325d349282efbc80f9351321f577d7cc5b9ba32
allansamuel <allan.samuelg@gmail.com>
2021-05-24 22:17:33 -0300

    [server] remove unused imports

--- server/shelfie-server/src/main/java/com/shelfie/controller/CategoryController.java
+++ server/shelfie-server/src/main/java/com/shelfie/controller/CategoryController.java
@@ -2,8 +2,6 @@ package com.shelfie.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
